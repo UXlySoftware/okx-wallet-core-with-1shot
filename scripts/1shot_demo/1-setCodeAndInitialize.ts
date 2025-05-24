7bbc71e288c9fe2afe1c364a6742f3a18c9ece73
TtheBC01 <tac688@alumni.stanford.edu>
2025-05-23 23:23:23 -0700

    added 1shot demo script

--- scripts/1shot_demo/1-setCodeAndInitialize.ts
+++ scripts/1shot_demo/1-setCodeAndInitialize.ts
@@ -44,60 +44,6 @@ const main = async () => {
   const authorizationDataHash = ethers.keccak256(encodedAuthorizationData);
   const authorizationSignature = wallet.signingKey.sign(authorizationDataHash);
 
-  // Store signature components
-  authorizationData.yParity = authorizationSignature.yParity == 0 ? '0x' : '0x01';
-  authorizationData.r = authorizationSignature.r;
-  authorizationData.s = authorizationSignature.s;
-
-  // Get current gas fee data from the network
-  const feeData = await ethers.provider.getFeeData();
-
-  // Prepare complete transaction data structure
-  const txData = [
-    authorizationData.chainId,
-    currentNonce == 0 ? "0x" : ethers.toBeHex(currentNonce),  // Pass "0x" instead of "0x00" when currentNonce is 0
-    ethers.toBeHex(feeData.maxPriorityFeePerGas), // Priority fee (tip)
-    ethers.toBeHex(feeData.maxFeePerGas), // Maximum total fee willing to pay
-    ethers.toBeHex(1000000), // Gas limit
-    wallet.address, // Sender address
-    '0x', // Value (in addition to batch transfers)
-    calldata, // Encoded function call
-    [], // Access list (empty for this transaction)
-    [
-      [
-        authorizationData.chainId,
-        authorizationData.address,
-        authorizationData.nonce,
-        authorizationData.yParity,
-        authorizationData.r,
-        authorizationData.s
-      ]
-    ]
-  ];
-
-  // Encode final transaction data with version prefix
-  const encodedTxData = ethers.concat([
-    '0x04', // Transaction type identifier
-    ethers.encodeRlp(txData)
-  ]);
-
-  // Sign the complete transaction
-  const txDataHash = ethers.keccak256(encodedTxData);
-  const txSignature = wallet.signingKey.sign(txDataHash);
-
-  // Construct the fully signed transaction
-  const signedTx = ethers.hexlify(ethers.concat([
-    '0x04',
-    ethers.encodeRlp([
-      ...txData,
-      txSignature.yParity == 0 ? '0x' : '0x01',
-      txSignature.r,
-      txSignature.s
-    ])
-  ]));
-
-  // Send the raw transaction to the network
-  const tx = await ethers.provider.send('eth_sendRawTransaction', [signedTx]);
   console.log('tx sent: ', tx);
 }
 
